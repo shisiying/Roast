e5a9fc226e18c455ad0f14998848058c966911c1
shisiying <shisiying@xhzyxed.cn>
2019-07-22 00:11:02 +0800

    feat:完成jsapi的编写

---
+++ resources/assets/js/api/cafe.js
@@ -0,0 +1,30 @@
+import {ROAST_CONFIG} from '../config/config.js';
+
+export default {
+    /**
+     * GEt /api/v1/cafes
+     */
+    getCafes:function () {
+        return axios.get(ROAST_CONFIG.API_URL+'/cafes');
+    }
+
+    /**
+     * Get /api/v1/cafes/{cafeID}
+     */
+    getCafe:function (cafeID) {
+        return axios.get(ROAST_CONFIG.API_URL+'/cafes/'+cafeID);
+    }
+
+    /**
+     * Post /api/v1/cafes
+     */
+    postAddNewCafe:function (name,address,city,state,zip) {
+        return axios.post(ROAST_CONFIG.API_URL+'/cafes',{
+            name:name,
+            address:address,
+            city:city,
+            state:state,
+            zip:zip
+        });
+    }
+}
